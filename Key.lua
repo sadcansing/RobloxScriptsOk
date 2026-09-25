b8ae2e6ae08aecb3ec4b5162c9dff020867681fc
RobloxScriptsOk <mashudushudu522@gmail.com>
2026-09-25 11:20:45 +0200

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "2580", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "Link", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/sadcansing/RobloxScriptsOk/refs/heads/main/LaRivals.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
